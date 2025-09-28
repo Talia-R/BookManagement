de9577ae01637e9399c5378173e2c60541d3553a
Talia-R <natalia.1067@hotmail.com>
2025-09-28 12:59:43 -0300

    refactor EStatus com descricao

--- backend/src/EStatus.java
+++ backend/src/EStatus.java
@@ -1,5 +1,15 @@
 public enum EStatus {
-    lido,
-    nãoLido,
-    iniciado;
+    lido("Concluido"),
+    nãoLido("Não lido"),
+    iniciado("Em andamento");
+
+    private String descricao;
+
+    private EStatus(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
 }
