644dcadb98b31926e0bb1ed99d324b022493f0e8
Talia-R <natalia.1067@hotmail.com>
2025-09-28 12:41:11 -0300

    init constructor

--- backend/src/Livro.java
+++ backend/src/Livro.java
@@ -4,6 +4,24 @@ public class Livro {
     private String nome;
     private Autor autor;
     private int totalPaginas;
-    private boolean status;
+    private EStatus status;
+
+    public Livro(String nome, int totalPaginas){
+        if(nome == null || nome.trim().length() < 2){
+            throw new IllegalArgumentException("Nome deve ser um campo preenchido com pelo menos dois caracter");
+        }
+        if(totalPaginas <= 0){
+            throw new IllegalArgumentException("O total de páginas deve ser maior que zero");
+        }
+        this.nome = nome;
+        this.totalPaginas = totalPaginas;
+    }
+
+    public Livro(String nome, int totalPaginas, Autor autor, EStatus status){
+        this(nome, totalPaginas);
+        this.autor = autor;
+        this.status = status;
+    }
+
 
 }
